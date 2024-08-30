3fd1c85b3a996ef4065521703e566a6b8b49e54a
sanudas2018 <sanu.das08103@gmail.com>
2024-08-30 22:02:36 +0600

    User regi: success with Try Catch method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,20 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     // User Registration Function
-    function UserRegistration(Request $request){
-        User::create([
-            'firstName' => $request->input('firstName'),
-            'lastName' => $request->input('lastName'),
-            'email' => $request->input('email'),
-            'mobile' => $request->input('mobile'),
-            'password' => $request->input('password'),
-        ]);
+    function UserRegistration(Request $request)
+    {
+        // User try catch 
+        try {
+            // input থেকে Data নিয়ে User model মাধ্যমে Data কে Database এ দিয়ে দিবে। 
+            User::create([
+                'firstName' => $request->input('firstName'),
+                'lastName' => $request->input('lastName'),
+                'email' => $request->input('email'),
+                'mobile' => $request->input('mobile'),
+                'password' => $request->input('password'),
+            ]);
 
-        
+            // User Registration হবে, পরে message show করার জন্য। এটি Front End থেকে ব্যাবহার করা যেতে পারে।
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User Registration Successfully'
+            ], 200);
+        } catch (Exception $error) {
+            //throw $error;
+            return response()->json([
+                'status' => 'failed',
+                // 'message' => 'User registration Failed'
+                'message' => $error->getMessage()
+            ], 200);
+        }
     }
 }
