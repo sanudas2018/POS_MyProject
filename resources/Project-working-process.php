642aecdf1bd485cbd8cdc2e117296334353ba19c
sanudas2018 <sanu.das08103@gmail.com>
2024-09-06 02:50:07 +0600

    Project Working Document

---
+++ resources/Project-working-process.php
@@ -0,0 +1,37 @@
+<!-- 
+ 1. Project Requirement & Features
+ 2. User Table
+ 3. Create New Project
+ -------------------------------------
+ 4. Create User Table Migration & Run 
+ 5. Create User Model
+ 6. Create Controller
+ 7. Listing Back End Point Controller & Routing (postman)
+
+ -------Back-End First Development Approach------
+ 8. User Registration Back-End Action
+ 9. User Login Bank-End Action
+ 10. User Logout Action
+ 11. Email Setup (SMTP Setup)
+ 12. OTP Send
+ 13. OTP Confirm
+ 14. Password Reset
+ 15. Read Profile Details
+ 16. Update Profile Details
+
+
+ Laravel Security:
+ 1. OWASP Laravel Security-
+ https://cheatsheetseries.owasp.org/cheatsheets/Laravel_Cheat_Sheet.html
+
+
+
+
+
+
+
+
+
+
+
+-->
